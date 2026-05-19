4d2d00051f1f2f96e1b31d8f3097e5d5dd4a5037
joaovitorcosta29 <joaovitorcostalima81@gmail.com>
2026-05-19 17:14:15 -0300

    inicializando o projeto

---
+++ src/main/java/com/front_end/bidding_front/BiddingFrontApplication.java
@@ -0,0 +1,13 @@
+package com.front_end.bidding_front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BiddingFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BiddingFrontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/front_end/bidding_front/BiddingFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.front_end.bidding_front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BiddingFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
